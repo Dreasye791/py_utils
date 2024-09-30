4e695981dd51ddc778e76c89041b14e45833d2df
dongzheng <dreasye@qq.com>
2024-09-30 09:39:24 +0800

    移除不必要的代码

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import csv
 import sql_utils
 
-# 打开 CSV 文件进行读取
 with open('source.csv', mode='r', newline='', encoding='utf-8') as file:
-    # 创建一个 CSV 读取器对象
     csv_reader = csv.reader(file)
 
-    # 读取 CSV 文件的标题行（可选）
-    headers = next(csv_reader)
-    # print("Headers:", headers)
+    # 读取 CSV 文件的标题行
+    # headers = next(csv_reader)
 
     alter_result = []
     update_result = []
 
-
-    def build_sql(table_name, column_name, column_length):
-        sql = 'alter table {} MODIFY {} VARCHAR2({});'.format(table_name, column_name, column_length)
-        return sql
-
-
-    # 遍历 CSV 文件中的每一行
     for row in csv_reader:
-        # 处理每一行的数据
-        # print("Row:", row)
         table_name = row[1]
         table_column = row[2]
         column_length = row[4]
